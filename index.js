6efb463a1d2c17f137ab577f65a53db51a58efb2
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-03 18:39:01 -0500

    Mentor help, added lat/lon API

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIp } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const ipWorkedCallback = (error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
-  console.log('It worked! Returned IP:' , ip);
-});
+  console.log('It worked! Returned ip:', ip);
+ fetchCoordsByIp(ip, coordsWorkedCallback);
+};
+
+const coordsWorkedCallback = (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:', coordinates);
+//  return coordinates;
+};
+
+// const runApp = () => {
+// fetchMyIP(ipWorkedCallback);
+// };
+
+fetchMyIP(ipWorkedCallback);
--- iss.js
+++ iss.js
@@ -12,21 +12,37 @@ const request = require('request'); // Put this at the top
 const fetchMyIP = (callback) => {
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
     if (error) {
-      callback(error, `${null} AN ERROR HAS OCCURED!`); // Print null if one occurred
-      
+      return callback(error, `${null} AN ERROR HAS OCCURED!`); // Print null if one occurred
     }
     if (response.statusCode !== 200) {   // if non-200 status, assume server error
-      const msg = (`Status Code ${response.statusCode} when fetching IP. Response: ${body}`, null);
-      callback(Error(msg), null); // CREATES A NEW ERROR OBJECT THAT WE CAN PASS AROUND
-      return;
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null); // CREATES A NEW ERROR OBJECT THAT WE CAN PASS AROUND
     }
-    const ip = JSON.parse(body).ip; // ADDED to make string into OBJECT! 
+    const json = JSON.parse(body); // ADDED to make string into OBJECT! // good syntax
     // ip NOT data!!!
-    callback(null, ip);
+    return callback(null, json.ip);
     // console.log(data); // COMMENT THIS OUT
     // if we get here, all's well and we got the data
   });
 };
 
+const fetchCoordsByIp = (ip, callback) => {
+  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, `${null} AN ERROR HAS OCCURED!`); // Print null if one occurred
+    }
+    if (response.statusCode !== 200) {   // if non-200 status, assume server error
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null); // CREATES A NEW ERROR OBJECT THAT WE CAN PASS AROUND
+    }
+    const json = JSON.parse(body);
+    // console.log(json.lat, json.lon);
+    const coordinates = {
+      lat: json.lat,
+      lon: json.lon
+    };
+    return callback(null, coordinates);
+  });
+};
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIp };
